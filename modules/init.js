9ca9cb040d317fa5ed43e206867ae0e79dc744a7
alexisrr11 <alexis.r4995@gmail.com>
2025-06-06 01:30:59 -0300

    Update init.js

--- modules/init.js
+++ modules/init.js
@@ -1,7 +1,7 @@
 import { leerData } from "./leerData.js";
 
 export async function initApp() {
-  const urls = ["/pacientes.json", "/medicos.json"];
+  const urls = ["../pacientes.json", "../medicos.json"];
   const sidebar = document.getElementById('sidebar');
   const toggleButton = document.getElementById('toggleSidebar');
   const contenedorFetch = document.querySelector(".contenedorJson");
